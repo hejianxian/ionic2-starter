aa629503f72d1ac9b83814a5f2698f762e1f612a
Hejx <531601727@qq.com>
2016-03-25 21:59:27 +0800

    add comment list

--- app/pages/detail/detail.ts
+++ app/pages/detail/detail.ts
@@ -22,7 +22,7 @@ export class DetailPage {
       this.params.get('id');
   }
 
-  onPageWillEnter () {
+  onPageDidEnter () {
       let _id = this.params.data.id;
       this.topicService.getDetailByID(_id).subscribe(
           data => {
@@ -35,3 +35,12 @@ export class DetailPage {
   }
   
 }
+
+// onPageLoaded - Runs when the page has loaded. This event only happens once per page being created and added to the DOM. If a page leaves but is cached, then this event will not fire again on a subsequent viewing. The onPageLoaded event is good place to put your setup code for the page.
+// onPageWillEnter - Runs when the page is about to enter and become the active page.
+// onPageDidEnter - Runs when the page has fully entered and is now the active page. This event will fire, whether it was the first load or a cached page.
+// onPageWillLeave - Runs when the page is about to leave and no longer be the active page.
+// onPageDidLeave - Runs when the page has finished leaving and is no longer the active page.
+// onPageWillUnload - Runs when the page is about to be destroyed and have its elements removed.
+// onPageDidUnload - Runs after the page has been destroyed and its elements have been removed.
+
